8a5cb8ef9a6fe428264791e51aa72a96d581f15b
Marcus Behel <marcus.behel@mindspring.com>
2021-12-17 23:10:11 -0500

    Updated cmake toolchain (cpp template) to find toolchains with different prefixes

--- templates/cpp/arpirobot-cross.cmake
+++ templates/cpp/arpirobot-cross.cmake
@@ -1,12 +1,52 @@
 if(CMAKE_HOST_WIN32)
-	set(HOMEDIR $ENV{UserProfile})
-	set(EXTENSION ".exe")
+    set(HOMEDIR $ENV{UserProfile})
+    set(EXTENSION ".exe")
 else()
-	set(HOMEDIR $ENV{HOME})
-	set(EXTENSION "")
+    set(HOMEDIR $ENV{HOME})
+    set(EXTENSION "")
 endif()
 
-set(target_host arm-linux-gnueabihf)
+set(POSSIBLE_PREFIXES
+    # Software float, 32-bit
+    arm-linux-gnueabi
+    armv6-rpi-linux-gnueabi
+    armv7-rpi2-linux-gnueabi
+    armv8-rpi3-linux-gnueabi
+    armv8-rpi4-linux-gnueabi
+
+    # Hardware float, 32-bit
+    arm-linux-gnueabihf
+    armv6-rpi-linux-gnueabihf
+    armv7-rpi2-linux-gnueabihf
+    armv8-rpi3-linux-gnueabihf
+    armv8-rpi4-linux-gnueabihf
+
+    # Software float, 64-bit
+    aarch64-linux-gnueabi
+    aarch64-rpi3-linux-gnueabi
+    aarch64-rpi4-linux-gnueabi
+
+    # Hardware float, 64-bit
+    aarch64-linux-gnueabihf
+    aarch64-rpi3-linux-gnueabihf
+    aarch64-rpi4-linux-gnueabihf
+)
+
+set(target_host "")
+foreach(POSSIBLE_PREFIX ${POSSIBLE_PREFIXES})
+    if(EXISTS "${HOMEDIR}/.arpirobot/toolchain/${POSSIBLE_PREFIX}")
+        set(target_host ${POSSIBLE_PREFIX})
+        break()
+    endif()
+endforeach()
+
+if("${target_host}" STREQUAL "")
+    message(FATAL_ERROR "ArPiRobot toolchain not found!")
+endif()
+
+message(STATUS "Found toolchain with prefix ${target_host}")
+
+
 set(standalone_toolchain ${HOMEDIR}/.arpirobot/toolchain)
 set(cc_compiler gcc)
 set(cxx_compiler g++)
