058f7ece39b4590189c48dca2b6780e44d5559db
Marcus Behel <marcus.behel@mindspring.com>
2024-01-28 15:04:34 -0500

    cpp template updated for using clang build mechanism

--- templates/cpp/arpirobot-aarch64-toolchain.cmake
+++ templates/cpp/arpirobot-aarch64-toolchain.cmake
@@ -6,19 +6,54 @@ else()
     set(EXTENSION "")
 endif()
 
-set(target_host "aarch64-unknown-linux-gnu")
-set(standalone_toolchain "${HOMEDIR}/.arpirobot/toolchain/aarch64")
-set(cc_compiler gcc)
-set(cxx_compiler g++)
+if(CMAKE_HOST_APPLE)
+    execute_process(COMMAND clang --version OUTPUT_VARIABLE CLANGOUT)
+    string(TOLOWER ${CLANGOUT} CLANGOUT)
+    string(FIND ${CLANGOUT} "apple clang" APPLECLANGPOS)
+    if(${APPLECLANGPOS} STREQUAL "-1")
+        # Clang in path is not apple clang. Assume user modified the path
+        set(CLANG clang)
+        set(CLANGPP clang++)
+    else()
+        # MacOS includes Apple Clang, which doesn't support lld
+        # Which is needed for cross compiling
+        # Need to use llvm clang instead, which is assumed to be installed by
+        # brew at /usr/local/opt/llvm/
+        set(CLANG /usr/local/opt/llvm/bin/clang)
+        set(CLANGPP /usr/local/opt/llvm/bin/clang++)
+    endif()
+else()
+    # Just use whatever is in the path elsewhere
+    set(CLANG clang)
+    set(CLANGPP clang++)
+endif()
 
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CROSSCOMPILING TRUE)
+SET(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_SYSROOT "${standalone_toolchain}/${target_host}/sysroot")
-set(CMAKE_C_COMPILER "${standalone_toolchain}/bin/${target_host}-${cc_compiler}${EXTENSION}" CACHE PATH "C Compiler")
-set(CMAKE_CXX_COMPILER "${standalone_toolchain}/bin/${target_host}-${cxx_compiler}${EXTENSION}" CACHE PATH "C++ Compiler")
+SET(TARGET aarch64-linux-gnu)
+set(CMAKE_SYSROOT "${HOMEDIR}/.arpirobot/sysroot/aarch64")
 
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+if(NOT EXISTS "${CMAKE_SYSROOT}")
+    message(FATAL_ERROR "Sysroot directory is missing.")
+endif()
+
+SET(CMAKE_C_COMPILER ${CLANG})
+SET(CMAKE_C_COMPILER_TARGET aarch64-linux-gnu)
+
+SET(CMAKE_CXX_COMPILER ${CLANGPP})
+SET(CMAKE_CXX_COMPILER_TARGET aarch64-linux-gnu)
+
+SET(CMAKE_ASM_COMPILER ${CLANG})
+SET(CMAKE_ASM_COMPILER_TARGET aarch64-linux-gnu)
+
+# Note: --sysroot automatically passed if CMAKE_SYSROOT is set
+# Note: -target is automatically passed by cmake as set above
+# Note: Linking with lld since it is cross linker natively
+#       Avoids needing gcc for target system just to link
+#       Thus, using clang and lld, no cross GNU toolchain is needed. Only sysroot.
+SET(SHARED_FLAGS "-fuse-ld=lld -Qunused-arguments")
+SET(CMAKE_C_FLAGS "${SHARED_FLAGS}" CACHE STRING "C compiler flags")
+SET(CMAKE_CXX_FLAGS "${SHARED_FLAGS}" CACHE STRING "C++ compiler flags")
+set(LINK_FLAGS "${SHARED_FLAGS} -fuse-ld=lld" CACHE STRING "Linker flags")
